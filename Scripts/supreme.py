e5740e3f31a7adb4b14015f363a3f5198a8ca53e
Bryan Tran <bryanvantran@ucla.edu>
2016-09-11 17:05:42 -0700

    removed early link option and eu

--- Scripts/supreme.py
+++ Scripts/supreme.py
@@ -25,40 +25,29 @@ keyword = True
 checkout_qty = int(input('How many products would you like to checkout? '))
 
 for input_counter, product in enumerate(range(checkout_qty)):
-    use_early_link = input('\n' + 'Do you have an early link for product ' + str(input_counter + 1) + '? [y/n] ')
-    if use_early_link.lower() in ['y', 'yes']:
-        link = input('Enter the link: ').strip()
-        early_links.append(link)
-        link_or_keyword.append(True)
-        
-        keyword_category.append(False)
-        keyword_model.append(False)
-        keyword_style.append(False)
-    else:
-        if keyword:
-            print('\n' + 'Example of a keyword search: ')
-            print('Category: accessories')
-            print('Model: hanes, socks')
-            print('Style: white', '\n')
-            keyword = False
-        
-        category = input('Enter category keyword: ').strip()
-        model = input('Enter model keywords seperated by commas (actual model must contain all keywords): ').strip().title()
-        style = input('Enter style (color) keyword: ').strip().title()
-        
-        if category == 'tops/sweaters' or category == 'tops' or category == 'sweaters':
-            category = 'tops_sweaters'
-        keyword_category.append(category)
-        keyword_model.append(model.split(','))
-        keyword_style.append(style)
-        
-        link_or_keyword.append(False)
-        early_links.append(False)
-    
+    if keyword:
+        print('\n' + 'Example of a keyword search: ')
+        print('Category: accessories')
+        print('Model: hanes, socks')
+        print('Style: white', '\n')
+        keyword = False
+
+    category = input('Enter category: ').strip()
+    model = input('Enter model keywords seperated by commas (actual model must contain all keywords): ').strip().title()
+    style = input('Enter style (color) keyword: ').strip().title()
+
+    if category == 'tops/sweaters' or category == 'tops' or category == 'sweaters':
+        category = 'tops_sweaters'
+    keyword_category.append(category)
+    keyword_model.append(model.split(','))
+    keyword_style.append(style)
+    link_or_keyword.append(False)
+    early_links.append(False)
+
     if input_counter == 0:
         print('\n' + 'Valid Clothing/Accessory Sizes: Small, Medium, Large, XLarge, N/A')
         print('If there is only one size, enter N/A', '\n')
-    
+
     size = input('Size: ').strip().title()
     sizes.append(size)
     matches.append(False)
@@ -105,11 +94,6 @@ if shipping_country not in country_lookup:
     sys.exit('Sorry, Supreme doesnt ship to your country')
 else:
     country_abbrv = country_lookup[shipping_country]
-    
-if shipping_country is not 'United States':
-    eu = True
-else:
-    eu = False
 
 if card_type.lower() in ['mastercard', 'master card', 'master']:
     card_ = 'master'
@@ -117,8 +101,6 @@ elif card_type.lower() == 'visa':
     card_ = 'visa'
 elif card_type.lower() == 'american express':
     card_ = 'american_express'
-elif card_type.lower() == 'solo' and eu is True:
-    card_ = 'solo'
 else:
     sys.exit('You must use a master, visa, solo (EU only) or an american express card')
 
@@ -130,7 +112,7 @@ def add_to_cart(session, url):
     soup = bs(response.text, "html.parser")
     product_name = soup.find('h1', {'itemprop': 'name'}).string
     print('\n' + 'Adding {} to cart...'.format(product_name))
-    
+
     session.headers.update({
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/52.0.2743.116 Safari/537.36',
@@ -141,10 +123,10 @@ def add_to_cart(session, url):
         'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
         'DNT': '1'
     })
-    
+
     form = soup.find('form', {'id': 'cart-addf'})
     csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
-    
+
     if sizes[counter].lower() == ('N/A').lower():
         size_input = form.find('input', {'id': 'size'})
     else:
@@ -155,19 +137,17 @@ def add_to_cart(session, url):
         size_id = size_input['value']
     else:
         print('Size is out of stock!')
-    
+
     if form is not None and size_id != '':
         cart = 'cart'
-        if eu:
-            cart = 'basket'
-        
+
         payload = {
             'utf8': '✓',
             'authenticity_token': form.find('input', {'name': 'authenticity_token'})['value'],
             'size': size_id,
             'commit': 'add to ' + cart
         }
-        
+
         headers = {
             'Accept': '*/*;q=0.5, text/javascript, application/javascript, application/ecmascript, application/x-ecmascript',
             'Origin': 'http://www.supremenewyork.com',
@@ -178,7 +158,7 @@ def add_to_cart(session, url):
             'X-CSRF-Token': csrf_token,
             'Accept-Encoding': 'gzip, deflate'
         }
-        
+
         session.post(base_url + form['action'], data=payload, headers=headers)
         products_in_cart = True
         print('Added {} to cart!'.format(product_name))
@@ -189,7 +169,7 @@ def checkout(session):
     response = session.get('https://www.supremenewyork.com/checkout')
     soup = bs(response.text, 'html.parser')
     form = soup.find('form', {'action': '/checkout'})
-    
+
     csrf_token = soup.find('meta', {'name': 'csrf-token'})['content']
     headers = {
         'Accept': 'text/html, */*; q=0.01',
@@ -198,7 +178,7 @@ def checkout(session):
         'Referer': 'https://www.supremenewyork.com/checkout',
         'Accept-Encoding': 'gzip, deflate, sdch, br'
     }
-    
+
     year = card_exp_year
     if len(card_exp_year) == 2:
         year = '20' + card_exp_year
@@ -226,25 +206,11 @@ def checkout(session):
         'hpcvv': '',
         'cnt': '1'
     }
-    
+
     checkout_payload = payload.copy()
-    
-    if eu:
-        payload['order[tel]'] = phone_number
-        payload['order[billing_address_2]'] = shipping_address_2
-        payload['order[billing_address_3]'] = shipping_apt_suite
-        del payload['store_credit_id']
-        del payload['order[billing_state]']
-    
+
     session.get('https://www.supremenewyork.com/checkout.js', data=payload, headers=headers)
-    
-    if eu:
-        payload['order[tel]'] = phone_number
-        checkout_payload['order[billing_address_2]'] = shipping_address_2
-        checkout_payload['order[billing_address_3]'] = shipping_apt_suite
-        del checkout_payload['order[billing_state]']
-    del checkout_payload['cnt']
-    
+
     headers = {
         'Origin': 'https://www.supremenewyork.com',
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -253,7 +219,7 @@ def checkout(session):
     }
 
     response = session.post('https://www.supremenewyork.com/checkout', data=checkout_payload, headers=headers)
-    
+
     if 'Your order has been submitted' in response.text:
         print('Checkout was successful, check for a confirmation email!')
     else:
@@ -292,56 +258,39 @@ search_session.headers.update({
 })
 
 for counter, i in enumerate(link_or_keyword):
-    if i:  # early_link
+    url = base_url + '/shop/all/' + keyword_category[counter] + '/'
+    print('Looking in ' + url)
+    while not matches[counter]:
+        print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
         try:
-            response1 = search_session.get(early_links[counter])
-            soup = bs(response1.text, 'html.parser')
+            response1 = search_session.get(url)
         except:
             print('Unable to connect to site...')
             if counter == checkout_qty - 1:
                 sys.exit()
             else:
                 continue
-        sold_out = soup.find('fieldset', {'id': 'add-remove-buttons'}).find('b')
-        if sold_out is not None:
-            print('Sorry, product is sold out!')
-        else:
-            add_to_cart(checkout_session, early_links[counter])
-            products_in_cart = True
-    else:  # keyword search
-        url = base_url + '/shop/all/' + keyword_category[counter] + '/'
-        print('Looking in ' + url)
-        while not matches[counter]:
-            print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
-            try:
-                response1 = search_session.get(url)
-            except:
-                print('Unable to connect to site...')
-                if counter == checkout_qty - 1:
-                    sys.exit()
-                else:
-                    continue
-            
-            soup1 = bs(response1.text, 'html.parser')
-            h1 = soup1.find_all('h1')
-            for i in h1:
-                for link in i.find_all('a', {'class': 'name-link'}, href=True):
-                    model = link.string
-                    in_link = True
-                    for keyword in keyword_model[counter]:
-                        if keyword not in model:
-                            in_link = False
-                            break
-                    if in_link and keyword_style[counter] == i.parent.find('p').string and matches[counter] == False:
-                        matches[counter] = True
-                        if i.parent.find('div', {'class': 'sold_out_tag'}) is None:
-                            add_to_cart(checkout_session, base_url + link['href'])
-                            break
-                        else:
-                            print('Sorry, product is sold out!')
-            if matches[counter] == False:
-                print('Could not find item. Trying again...')
-                time.sleep(0.1)
+
+        soup1 = bs(response1.text, 'html.parser')
+        h1 = soup1.find_all('h1')
+        for i in h1:
+            for link in i.find_all('a', {'class': 'name-link'}, href=True):
+                model = link.string
+                in_link = True
+                for keyword in keyword_model[counter]:
+                    if keyword not in model:
+                        in_link = False
+                        break
+                if in_link and keyword_style[counter] in i.parent.find('p').string and matches[counter] == False:
+                    matches[counter] = True
+                    if i.parent.find('div', {'class': 'sold_out_tag'}) is None:
+                        add_to_cart(checkout_session, base_url + link['href'])
+                        break
+                    else:
+                        print('Sorry, product is sold out!')
+        if matches[counter] == False:
+            print('Could not find item. Trying again...')
+            time.sleep(0.1)
 
 if products_in_cart:
     checkout(checkout_session)
